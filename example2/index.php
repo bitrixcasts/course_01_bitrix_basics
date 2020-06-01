0f437c23b9cef97b82c351ffe12906b45f337230
Mikhail Volkov <m.volkov@qsoft.ru>
2020-06-01 18:27:20 +0300

    Урок 06 Урок 06. Куда писать свой код в Bitrix Урок 06 ч.2. Установка composer в Bitrix Урок 06 ч.3. Место для отладки в Bitrix

--- example2/index.php
+++ example2/index.php
@@ -1,7 +1,6 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
 $APPLICATION->SetTitle("Наша супер страница");
-var_dump(\Bitrix\Main\Config\Configuration::getValue('exception_handling'));
 ?>
 Hello World 2
 <?php
