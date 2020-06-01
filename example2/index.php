36ccb9af0e91a0c60a3024232a3d7ef453341b58
Mikhail Volkov <m.volkov@qsoft.ru>
2020-06-01 18:13:09 +0300

    Урок 05 Урок 05. Конфигурирование Bitrix

--- example2/index.php
+++ example2/index.php
@@ -1,6 +1,7 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
 $APPLICATION->SetTitle("Наша супер страница");
+var_dump(\Bitrix\Main\Config\Configuration::getValue('exception_handling'));
 ?>
 Hello World 2
 <?php
